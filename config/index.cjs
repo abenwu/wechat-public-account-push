50e6d5db8e7d7486cab2c8aa0f4ec987f38ca5a5
abenwu <115515841+abenwu@users.noreply.github.com>
2022-10-11 14:28:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,7 +42,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0OHhC9j_Hb0YGLRh1Y3ikOQ_ylF33fviHUxh0QklZxk',
 
   CALLBACK_USERS: [
     {
