d733ab90fb2fdb551a1b53de1d1ed59bf4fd5621
abenwu <115515841+abenwu@users.noreply.github.com>
2022-10-11 14:23:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,27 +25,28 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0OHhC9j_Hb0YGLRh1Y3ikOQ_ylF33fviHUxh0QklZxk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '10-15',
+      horoscopeDate: '11-15',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '小邹总', year: '2000', date: '10-15',
+          type: '*生日', name: '宝贝', year: '2000', date: '10-15',
         },
-        ,
-        {
-                 },
-      ],
+                  ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-07-16' },
-            ],
-   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+              ],
+    },
+  ],
+
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: 'obbjO6tb6rzQUXyCO3bcXenIP9oM',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
     }
